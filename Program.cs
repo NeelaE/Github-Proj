b88f63b245a488c0c2207efaf904036fe711d1e6
Neela <neelaeswaran.e@gmail.com>
2026-02-05 21:56:29 +0530

    changes

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+       
+        Console.WriteLine(Printer(5, 5));
+        Console.WriteLine(Printer(15, 5));
+        Console.WriteLine(Printer(5, 5));
+
+    }
+
+    public static int Printer(int a, int b)
+    {
+        int c = a + b;
+        return c;
+
+    }
+}
+
+    
